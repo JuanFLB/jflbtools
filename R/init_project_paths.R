f771ea7ea2a778d2aa71b6a37bd857153d2eb338
JuanFLB <jflbph@gmail.com>
2025-08-05 11:03:49 +0200

    Update README and documentation. Correct uncomented from "init_project_paths.R", needs revisiting and polishing

--- R/init_project_paths.R
+++ R/init_project_paths.R
@@ -1,38 +1,42 @@
-init_project_paths <- function(doc_name = NULL, 
+# This is an ongoing work.
+# Currently only runs upon rendering unless doc_name specified
+
+
+init_project_paths <- function(doc_name = NULL,
                                subfolders = c("rds", "figs", "tables")) {
-  
+
   base_dir <- here::here("data/results")
-  
+
   if (is.null(doc_name)) {
     doc_name <- fs::path_ext_remove(fs::path_file(knitr::current_input()))
   }
-  
+
   out_paths <- fs::path(base_dir, doc_name, subfolders)
   purrr::walk(out_paths, fs::dir_create)
-  
+
   paths <- purrr::set_names(out_paths, subfolders)
   assign("RESULT_PATHS", paths, envir = .GlobalEnv)
-  
+
   message(glue::glue("Initialized output folders under: data/results/{doc_name}/"))
   return(invisible(paths))
 }
 
-# Use it as
-
-#| label: setup-folders
-#| eval: true
-#| include: false
-init_project_paths()
-
-# It will generate
-
-results/
-  └── analysis1/
-    ├── rds/
-    ├── figs/
-    └── tables/
-
-# save as
-
-readr::write_rds(my_data, file = fs::path(RESULT_PATHS["rds"], "processed_data.rds"))
-ggsave("plot1.png", path = RESULT_PATHS["figs"], plot = p)
+# # Use it as
+#
+# #| label: setup-folders
+# #| eval: true
+# #| include: false
+# init_project_paths()
+#
+# # It will generate
+#
+# results/
+#   └── analysis1/
+#     ├── rds/
+#     ├── figs/
+#     └── tables/
+#
+# # save as
+#
+# readr::write_rds(my_data, file = fs::path(RESULT_PATHS["rds"], "processed_data.rds"))
+# ggsave("plot1.png", path = RESULT_PATHS["figs"], plot = p)
